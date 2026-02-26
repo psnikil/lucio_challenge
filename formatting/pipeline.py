64e89d63198decc948ad14795609189f0e5e1bc5
psnikil <nikilps36@gmail.com>
2026-02-26 22:55:55 +0000

    feat: Add `llama-cpp-python`, `huggingface-hub`, and `sentence-transformers` dependencies, updating the lock file, project configuration, and pipeline logic. TODO add tracing to the workers

--- formatting/pipeline.py
+++ formatting/pipeline.py
@@ -9,21 +9,43 @@ import collections
 import asyncio
 import zlib
 import orjson
+from llama_cpp import Llama
+import faiss
+import numpy as np
+from typing import List, Sequence, Optional
+from sentence_transformers import SentenceTransformer
+from dataclasses import dataclass
+import torch
+os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
 
+torch.cuda.empty_cache()
 ROOT = Path("./data/Testing_Set")
-NUM_WORKERS = 8
+EMBEDDER_MODEL_PATH=Path("./models/embedders")
+NUM_WORKERS = mp.cpu_count() or 8
 DOC_BATCH = 1
 QUEUE_MAX = 65536
-CHUNK_SIZE = 800
-CHUNK_OVERLAP = 120
+USE_COMPRESSION = False
+CHUNK_SIZE = 1500
+CHUNK_OVERLAP = 200
+CHUNKS_PER_MESSAGE = 4096
+PAGE_RANGE = 25
+BATCH_TEXT_SIZE = 10000
+# Metrics
+ENABLE_FINE_METRICS = True                 # set True if you really need 
+
+@dataclass
+class PageRangeJob:
+    path: str
+    start_page: int
+    end_page: int  # exclusive
 
 """ -----------Timer functions------------ """
 
 def now():
     return time.perf_counter()
 
-def report(metrics_q, name, dt, n=1):
-    metrics_q.put((name, dt, n))
+def report(metrics_q, name, dt, n=1,worker_id=None):
+    metrics_q.put((name, dt, n,worker_id))
 
 def collect_metrics(metrics_q, done_event):
     # this calculates the whole time,
@@ -52,7 +74,7 @@ def collect_metrics(metrics_q, done_event):
 
 
 """ -----------Text Chunking------------ """
-
+# TODO: needs to yield an object you can embed
 def chunk_text(text, size=800, overlap=120):
     i = 0
     n = len(text)
@@ -60,93 +82,136 @@ def chunk_text(text, size=800, overlap=120):
         yield text[i:i+size]
         i += size - overlap
 
-def report(metrics_q, name, dt, n=1, worker_id=None):
-    metrics_q.put((name, dt, n, worker_id))
+def serialize_chunks(chunks: Sequence[str]) -> bytes:
+    """
+    Serialise a sequence of chunks for IPC.
+
+    This is factored out so you can easily switch between:
+      - plain pickled Python lists (no compression)
+      - orjson + optional zlib
+    """
+    if USE_COMPRESSION:
+        return zlib.compress(orjson.dumps(chunks))
+    else:
+        # plain JSON bytes; mp.Queue will still pickle this bytes object
+        return orjson.dumps(chunks)
+
+
+def deserialize_chunks(payload: bytes) -> List[str]:
+    if USE_COMPRESSION:
+        return orjson.loads(zlib.decompress(payload))
+    else:
+        return orjson.loads(payload)
+
+
 
 def parse_worker(worker_id, in_q, out_q, metrics_q):
     # os.sched_setaffinity(0, {worker_id})
 
     while True:
-        paths = in_q.get()
-        if paths is None:
+        jobs = in_q.get()
+        if jobs is None:
             break
 
         batch_chunks = []
-        t0 = time.perf_counter()
+        total_chunks = 0
+        # t0 = now()
 
-        for path in paths:
-            t_open = time.perf_counter()
-            doc = pymupdf.open(path)
-            report(metrics_q, "pdf_open", time.perf_counter() - t_open, worker_id=worker_id)
-
-            t_text = 0.0
-            t_chunk = 0.0
-
-            for page in doc:
-                t_get = time.perf_counter()
+        for job in jobs:
+            # t_text = 0.0
+            # t_open = now()
+            doc = pymupdf.open(job.path)
+            batch_text = ""
+            for page_idx in range(job.start_page, job.end_page):
+                page = doc.load_page(page_idx)
                 text = page.get_text("text")
-                t_text += time.perf_counter() - t_get
+                # if ENABLE_FINE_METRICS:
+                #     report(metrics_q, "pdf_open", time.perf_counter() - t_open, worker_id=worker_id)
+                # t_text += time.perf_counter() - t_open
+                
+                # t_chunk = 0.0
+                # t_c = now()
+                batch_text += text
+                if len(batch_text) > BATCH_TEXT_SIZE:
+                    for c in chunk_text(batch_text):
+                        batch_chunks.append(c)
+                        total_chunks += 1
 
-                t_c = time.perf_counter()
-                for c in chunk_text(text):
-                    batch_chunks.append(c)
-                t_chunk += time.perf_counter() - t_c
+                        if total_chunks % CHUNKS_PER_MESSAGE == 0:
+                            # t_ser = now()
+                            payload = serialize_chunks(batch_chunks)
+                            # if ENABLE_FINE_METRICS:
+                            #     report(metrics_q, "ipc_serialize", now() - t_ser, len(batch_chunks), worker_id=worker_id)
+                            out_q.put(payload)
+                            batch_chunks = []
+                    # t_chunk += now() - t_c
 
             doc.close()
-            report(metrics_q, "text_extract", t_text, worker_id=worker_id)
-            report(metrics_q, "chunking", t_chunk, worker_id=worker_id)
+            # if ENABLE_FINE_METRICS:
+            #     report(metrics_q, "text_extract", t_text, worker_id=worker_id)
+            #     report(metrics_q, "chunking", t_chunk, worker_id=worker_id)
+        
+        if batch_chunks:
+            # t_ser = now()
+            payload = serialize_chunks(batch_chunks)
+            # if ENABLE_FINE_METRICS:
+            #     report(metrics_q, "ipc_serialize", now() - t_ser, len(batch_chunks), worker_id=worker_id)
+            out_q.put(payload)
+            batch_chunks = []
 
-        t_ser = time.perf_counter()
-        payload = zlib.compress(orjson.dumps(batch_chunks))
-        report(metrics_q, "ipc_serialize", time.perf_counter() - t_ser, len(batch_chunks), worker_id=worker_id)
-
-        out_q.put(payload)
-
-        report(metrics_q, "parse_batch_total", time.perf_counter() - t0, len(batch_chunks), worker_id=worker_id)
+        # if ENABLE_FINE_METRICS:
+        #     report(metrics_q, "parse_batch_total", now() - t0, total_chunks, worker_id=worker_id)
 
 
 async def embed_sink(out_q, metrics_q):
     # Doing this to test the pipeline without the embedding model
     loop = asyncio.get_running_loop()
-    if True:
+
+    TEST_MODE = False    
+    if TEST_MODE:
         while True:
             payload = await loop.run_in_executor(None, out_q.get)
             if payload is None:
                 break
-            t_deser = time.perf_counter()
-            chunks = orjson.loads(zlib.decompress(payload))
-            report(metrics_q, "ipc_deserialize", time.perf_counter() - t_deser, len(chunks))
+            # t_deser = now()
+            chunks = deserialize_chunks(payload)
+            # if ENABLE_FINE_METRICS:
+            #     report(metrics_q, "ipc_deserialize", now() - t_deser, len(chunks))
     else:
-        llm = Llama(
-            model_path="./models/embedding.gguf",
-            n_threads=8,
-            n_batch=2048,
-            embedding=True,
-            use_mmap=True,
-            use_mlock=True,
-        )
+        llm = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", 
+                            device="cuda",
+                            cache_folder=EMBEDDER_MODEL_PATH,
+                            model_kwargs={"dtype": torch.bfloat16})
+        dim = len(llm.encode(["test"])[0])
+        print(dim)
+        index = faiss.IndexHNSWFlat(dim, 32)
+        index.hnsw.efConstruction = 200
+
+        print(f"FAISS index dim={dim}")
 
         loop = asyncio.get_running_loop()
-
         while True:
             payload = await loop.run_in_executor(None, out_q.get)
             if payload is None:
                 break
 
             t_deser = time.perf_counter()
-            chunks = orjson.loads(zlib.decompress(payload))
-            report(metrics_q, "ipc_deserialize", time.perf_counter() - t_deser, len(chunks))
 
+            chunks = deserialize_chunks(payload)
+            # report(metrics_q, "ipc_deserialize", time.perf_counter() - t_deser, len(chunks))
+            # print(len(chunks[0]))
+            # print(chunks)
+            # break
             t_embed = time.perf_counter()
-            embeddings = await loop.run_in_executor(
-                None,
-                lambda: llm.create_embedding(chunks)
-            )
-            report(metrics_q, "embedding", time.perf_counter() - t_embed, len(chunks))
+            embs = await loop.run_in_executor(None, lambda: llm.encode(chunks, batch_size=512, show_progress_bar=True))
+            vectors = np.array(embs, dtype="float32")
+            # report(metrics_q, "embedding", time.perf_counter() - t_embed, len(chunks))
 
-            t_sink = time.perf_counter()
-            # TODO: write to RAM vector DB (FAISS / Qdrant / custom mmap store)
-            report(metrics_q, "db_sink", time.perf_counter() - t_sink, len(chunks))
+            t_db = time.perf_counter()
+            index.add(vectors)
+            # report(metrics_q, "faiss_add", time.perf_counter() - t_db, len(chunks))
+
+        print("FAISS index size:", index.ntotal)
 
 
 def sink_process_entry(out_q, metrics_q):
@@ -156,11 +221,10 @@ def main():
     mp.set_start_method("spawn")
     metrics_q = mp.Queue()
 
-    done_event = mp.Event()
-    collector = mp.Process(target=collect_metrics, args=(metrics_q, done_event))
-    collector.start()
+    # done_event = mp.Event()
+    # collector = mp.Process(target=collect_metrics, args=(metrics_q, done_event))
+    # collector.start()
 
-    start_time = time.perf_counter()
     in_q = mp.Queue(maxsize=QUEUE_MAX)
     out_q = mp.Queue(maxsize=QUEUE_MAX)
 
@@ -177,16 +241,28 @@ def main():
 
     for w in workers:
         w.start()
+    
+    start_time = now()
 
     batch = []
     for root, _, files in os.walk(ROOT):
-        print('the files are', files)
         for f in files:
             if f.endswith(".pdf"):
-                batch.append(os.path.join(root, f))
-                if len(batch) == DOC_BATCH:
-                    in_q.put(batch)
-                    batch = []
+                path = os.path.join(root, f)
+                doc = pymupdf.open(path)
+                n_pages = len(doc)
+                doc.close()
+                if n_pages <= 25:
+                    in_q.put([PageRangeJob(path, 0, n_pages)])
+                else:
+                    jobs = []
+                    for start in range(0, n_pages, PAGE_RANGE):
+                        end = min(start + PAGE_RANGE, n_pages)
+                        jobs.append(PageRangeJob(path, start, end))
+                        in_q.put([PageRangeJob(path, start, end)])
+                # if len(batch) == DOC_BATCH:
+                #     in_q.put(batch)
+                #     batch = []
 
     if batch:
         in_q.put(batch)
@@ -202,8 +278,8 @@ def main():
 
     print("Total time: ", time.perf_counter() - start_time)
 
-    done_event.set()
-    collector.join()
+    # done_event.set()
+    # collector.join()
 
 
 
